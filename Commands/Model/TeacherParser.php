410e65167df2c5b9ccdc7a97f93102e091a50c17
MaxNovik <novik.kor@gmail.com>
2018-05-28 12:57:06 +0300

    Some initial code testing

---
+++ Commands/Model/TeacherParser.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Longman\TelegramBot\Commands\Model;
+
+class TeacherParser
+{
+    public static function parse(string $url): string
+    {
+        return __FILE__;
+    }
+}
