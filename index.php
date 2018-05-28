4cceef4426dc8f8c3f936cf7bd2af85d6e4c173d
MaxNovik <novik.kor@gmail.com>
2018-05-28 12:41:57 +0300

    Simplified index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Longman\TelegramBot\Exception\TelegramException;
 use Longman\TelegramBot\Telegram;
 
 try {
@@ -12,18 +11,9 @@ try {
         'bot_name'
     );
     $telegram->addCommandsPath(__DIR__ . '/Commands');
-    
+
     // Handle telegram webhook request
     $telegram->handle();
-    
-    return [
-        'statusCode' => 200,
-        'body' => $telegram->getLastCommandResponse()->__toString(),
-    ];
-} catch (TelegramException $e) {
-    
-    return [
-        'statusCode' => 500,
-        'body' => $e->getMessage(),
-    ];
+} catch (Exception $e) {
+    echo "<pre>$e</pre>";
 }
